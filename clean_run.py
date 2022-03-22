1a1de45c05d63bd4c5c449be16d4b9a996aa5c09
Giang <55189671+qdgiang@users.noreply.github.com>
2022-03-22 17:18:56 +0700

    Prepare for application

--- clean_run.py
+++ clean_run.py
@@ -4,59 +4,37 @@ from collections import deque
 import cv2 as cv
 import mediapipe as mp
 from helper import *
+class HandGesture():
+    def __init__(self):
+        self.previous_action_id = 0
+        self.current_action_id = 0
+        self.auto_mode = False
+        self.light_mode = False
 
-cam_width = 960
-cam_height = 540
-min_detection_confidence  = 0.7
-min_tracking_confidence = 0.7
-mpDraw = mp.solutions.drawing_utils
+        self.use_brect = True
+        self.cap_width = 620
+        self.cap_height = 400
+        self.min_detection_confidence  = 0.7
+        self.min_tracking_confidence = 0.7
 
-previous_action_id = 0
-current_action_id = 0
-auto_mode = False
-light_mode = False
-def main():
+        self.cap_device = 0
+        self.cap = cv.VideoCapture(self.cap_device)
+        self.cap.set(cv.CAP_PROP_FRAME_WIDTH, self.cap_width)
+        self.cap.set(cv.CAP_PROP_FRAME_HEIGHT, self.cap_height)
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(
+        static_image_mode= False,
+            max_num_hands=1,
+            min_detection_confidence=self.min_detection_confidence,
+            min_tracking_confidence=self.min_tracking_confidence,
+        )
 
-    global previous_action_id
-    global current_action_id
-    global auto_mode
-    global light_mode
-    
-    cap_device = 0
-    cap_width = cam_width
-    cap_height = cam_height
+        self.keypoint_classifier = KeyPointClassifier()
+        self.action_list = ["stop", "forward", "backward", "go left", "go right", "spin right", "spin left", "forward faster", "do nothing", "change mode", "change light"]
+        self.keypoint_classifier_labels = ["upward fist", "normal fist", "reverse fist", "palm", "reverse palm", "thumb left", "thumb right", "OK", "index up"]
 
-    use_static_image_mode = False
-    use_brect = True
-
-    cap = cv.VideoCapture(cap_device)
-    cap.set(cv.CAP_PROP_FRAME_WIDTH, cap_width)
-    cap.set(cv.CAP_PROP_FRAME_HEIGHT, cap_height)
-
-    mp_hands = mp.solutions.hands
-    hands = mp_hands.Hands(
-        static_image_mode=use_static_image_mode,
-        max_num_hands=1,
-        min_detection_confidence=min_detection_confidence,
-        min_tracking_confidence=min_tracking_confidence,
-    )
-
-    keypoint_classifier = KeyPointClassifier()
-    #point_history_classifier = PointHistoryClassifier()
-
-    action_list = ["stop", "forward", "backward", "go left", "go right", 
-    "spin right", "spin left", "forward faster", "do nothing", "change mode", "change light"]
-    keypoint_classifier_labels = ["upward fist", "normal fist", "reverse fist", "palm", 
-    "reverse palm", "thumb left", "thumb right", "OK", "index up"]
-    while True:
-
-        ##################################################
-        key = cv.waitKey(10)
-        if key == 27:  # ESC
-            break
-        ret, image = cap.read()
-        if not ret:
-            break
+    def main(self):
+        _, image = self.cap.read()
         image = cv.flip(image, 1) 
         debug_image = copy.deepcopy(image)
 
@@ -64,13 +42,13 @@ def main():
         image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
         image.flags.writeable = False
-        results = hands.process(image)
+        results = self.hands.process(image)
         image.flags.writeable = True
 
         ####################################################################
         if results.multi_hand_landmarks is not None:
             for hand_landmarks, handedness in zip(results.multi_hand_landmarks,
-                                                  results.multi_handedness):
+                                                results.multi_handedness):
                 # Calculation of rectangle boundaries
                 brect = calc_bounding_rect(debug_image, hand_landmarks)
                 
@@ -82,79 +60,85 @@ def main():
                     landmark_list)
 
                 # Hand sign classification
-                hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
-                cv.putText(debug_image, "Detected shape: " + keypoint_classifier_labels[hand_sign_id], 
+                hand_sign_id = self.keypoint_classifier(pre_processed_landmark_list)
+                cv.putText(debug_image, "Detected shape: " + self.keypoint_classifier_labels[hand_sign_id], 
                 (10,30), 
                 cv.FONT_HERSHEY_DUPLEX, 
                 0.5, (0, 0, 0), 1, cv.LINE_AA)
-                previous_action_id = current_action_id
-                current_action_id = getActionAndMode(hand_sign_id, hand_landmarks.landmark[0].x)
+                self.previous_action_id = self.current_action_id
+                self.current_action_id = self.getActionAndMode(hand_sign_id, hand_landmarks.landmark[0].x)
 
                 ## Draw result
-                debug_image = draw_bounding_rect(use_brect, debug_image, brect)
+                debug_image = draw_bounding_rect(self.use_brect, debug_image, brect)
                 debug_image = draw_landmarks(debug_image, landmark_list)
                 debug_image = draw_label(
                     debug_image,
                     brect,
                     handedness,
-                    action_list[current_action_id],
+                    self.action_list[self.current_action_id],
                     #keypoint_classifier_labels[hand_sign_id],
-                    auto_mode
+                    self.auto_mode
                 )
         else:
-            current_action_id = 8 # do nothing
+            self.current_action_id = 8 # do nothing
             cv.putText(debug_image, "Detected shape: None", 
                 (10,30), 
                 cv.FONT_HERSHEY_DUPLEX, 
                 0.5, (0, 0, 0), 1, cv.LINE_AA)
 
-        current_mode = "Auto" if auto_mode else "Manual"
-        current_light = "On" if light_mode else "Off"
+        current_mode = "Auto" if self.auto_mode else "Manual"
+        current_light = "On" if self.light_mode else "Off"
         cv.putText(debug_image, "Current Mode: " + current_mode, (10,60), cv.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1, cv.LINE_AA)
         cv.putText(debug_image, "Car Light: " + current_light, (10,90), cv.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1, cv.LINE_AA)
 
-        # SEND TO PI ThE CURRENT_ACTION_ID
-        #if not auto_mode:
+        # SEND TO PI ThE self.CURRENT_ACTION_ID
+        #if not self.auto_mode:
 
 
-        ####################################################################
-        cv.imshow('Hand Gesture Recognition', debug_image)
 
-    cap.release()
+        ##########################################################################
+        return debug_image
+
+    def getActionAndMode(self, hand_sign_id, hand_place):
+        if hand_sign_id == 0 or hand_sign_id == 1: # UPWARD FIST OR NORMAL FIST
+            if hand_place < 0.33: # LEFT SCREEN FIST
+                return 6 # spin left
+            elif hand_place > 0.66: # RIGHT SCREEN FIST
+                return 5 # spin right
+            else:
+                if hand_sign_id == 0: # MIDDLE SCREEN UPWARD FIST
+                    return 1 # normal forward
+                else: # MIDDLE SCREEN NORMAL FIST
+                    return 7 # fast forward
+        elif hand_sign_id == 2: # REVERSE FIST
+            return 2 # backward
+        elif hand_sign_id == 3: # PALM
+            return 0 # stop
+        elif hand_sign_id == 4: # REVERSE PALM
+            return 8 # do nothing
+        elif hand_sign_id == 5: # THUMB LEFT
+            return 3 # go left
+        elif hand_sign_id == 6: # THUMB RIGHT
+            return 4 # go right
+        elif hand_sign_id == 7: # OK
+            if (self.previous_action_id != 9): # if change mode first time
+                self.auto_mode = not self.auto_mode
+            return 9 # change mode
+        elif hand_sign_id == 8: # INDEX UP
+            if (self.previous_action_id != 10): # if change light first time
+                self.light_mode = not self.light_mode
+            return 10 # change light
+
+if __name__ == "__main__":
+    Hand_Object = HandGesture()
+    while True:
+        key = cv.waitKey(1)
+        if key == 27:
+            break
+        image = Hand_Object.main()
+        #cv.imwrite('F:\\Hung Luu\\BK_19_22\\3_Junior\\212\\DMC\\App Design\\Self-drviving-Car_App\\images\\hand_gesture_images\\cache.jpeg',
+                    #image)
+        cv.imshow('Hand Gesture Recognition', image)
+
+    Hand_Object.cap.release()
     cv.destroyAllWindows()
-
-def getActionAndMode(hand_sign_id, hand_place):
-    global auto_mode
-    global light_mode
-    global previous_action_id
-    if hand_sign_id == 0 or hand_sign_id == 1: # UPWARD FIST OR NORMAL FIST
-        if hand_place < 0.33: # LEFT SCREEN FIST
-            return 6 # spin left
-        elif hand_place > 0.66: # RIGHT SCREEN FIST
-            return 5 # spin right
-        else:
-            if hand_sign_id == 0: # MIDDLE SCREEN UPWARD FIST
-                return 1 # normal forward
-            else: # MIDDLE SCREEN NORMAL FIST
-                return 7 # fast forward
-    elif hand_sign_id == 2: # REVERSE FIST
-        return 2 # backward
-    elif hand_sign_id == 3: # PALM
-        return 0 # stop
-    elif hand_sign_id == 4: # REVERSE PALM
-        return 8 # do nothing
-    elif hand_sign_id == 5: # THUMB LEFT
-        return 3 # go left
-    elif hand_sign_id == 6: # THUMB RIGHT
-        return 4 # go right
-    elif hand_sign_id == 7: # OK
-        if (previous_action_id != 9): # if change mode first time
-            auto_mode = not auto_mode
-        return 9 # change mode
-    elif hand_sign_id == 8: # INDEX UP
-        if (previous_action_id != 10): # if change light first time
-            light_mode = not light_mode
-        return 10 # change light
-
-if __name__ == '__main__':
-    main()
